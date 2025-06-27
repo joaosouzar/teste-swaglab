9e3f5268a2390d829e39141a6d85705d01850f62
João Ribeiro <jmanoel84@gmail.com>
2025-06-27 23:49:05 +0100

    atualizando teste de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,40 +1,41 @@
 //cenário de teste
 describe('login', () => {
   //Caso de teste
-  it('Login com credenciais válidas', () => {
+  it('Login com sucesso', () => {
     cy.visit('https://www.saucedemo.com/v1/index.html')
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('#login-button').click()
   })
 
-  it('Login username inválido', () => {
+  it('Login com username em letras maiúsculas', () => {
     cy.visit('https://www.saucedemo.com/v1/index.html')
-    cy.get('[data-test="username"]').type('user_qualquer')
+    cy.get('[data-test="username"]').type('STANDARD_USER')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('#login-button').click()    
   })
 
-  it('Login com senha inválida', () => {
-    cy.visit('https://www.saucedemo.com/v1/index.html')
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('pass_qualquer')
-    cy.get('#login-button').click()    
-  })
-
-  it('Login com username vazio', () => {
+  it('Login com username em branco', () => {
     cy.visit('https://www.saucedemo.com/v1/index.html')
     cy.get('[data-test="username"]').type('{enter}')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('#login-button').click()    
   })
 
-  it('Login com senha vazia', () => {
+    it('Login com password em branco', () => {
     cy.visit('https://www.saucedemo.com/v1/index.html')
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type(' ')
     cy.get('#login-button').click()    
   })
+
+  it('Login com password inválida', () => {
+    cy.visit('https://www.saucedemo.com/v1/index.html')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('pass_qualquer')
+    cy.get('#login-button').click()    
+  })
+
 })
   
 
